098e111fd0ac4e81fe691fd9b61815a255f110a3
SivarajRadhakrishnan <sivaraj.radhakrishnan@bankbazaar.com>
2020-12-08 12:31:25 +0530

    Abstraction: Session1

---
+++ src/com/javademo/abstraction/AccountType.java
@@ -0,0 +1,13 @@
+package com.javademo.abstraction;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sivaraj
+ * Date: 8/12/20
+ * Time: 12:20 PM
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public enum AccountType {
+    SAVINGS,
+    CURRENT
+}
---
+++ src/com/javademo/abstraction/Customer.java
@@ -0,0 +1,19 @@
+package com.javademo.abstraction;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sivaraj
+ * Date: 8/12/20
+ * Time: 12:18 PM
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public class Customer {
+    String firstName;
+    String middleName;
+    String lastName;
+    String dob;
+    String address;
+    long mobile;
+    String panNumber;
+    String aadhaarNumber;
+}
